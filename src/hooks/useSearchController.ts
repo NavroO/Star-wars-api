8215e8de46e48eb3c25b21060badd248940ebc1f
Navro <tomeknawrocki2000@gmail.com>
2023-04-24 17:59:57 +0200

    Try catch small fix

--- src/hooks/useSearchController.ts
+++ src/hooks/useSearchController.ts
@@ -12,97 +12,97 @@ export const useSearchController = () => {
         setLoading(true);
         setSearchResult(null);
 
-        if (searchType == "population") {
-            const response = await fetch(`https://swapi.dev/api/planets/`);
+        try {
+            if (searchType == "population") {
+                const response = await fetch(`https://swapi.dev/api/planets/`);
+                const data = await response.json();
+
+                const filteredPlanets = data.results.filter((planet: any) => {
+                    const planetPopulation = parseInt(planet.population);
+
+                    return planetPopulation >= (minValue ?? 0) && (!maxValue || planetPopulation <= maxValue);
+                });
+
+                const planetsData = await Promise.all(
+                    filteredPlanets.map(async (planet: any) => {
+                        const response = await fetch(planet.url);
+
+                        if (!response.ok) {
+                            throw new Error("Network response was not ok");
+                        }
+
+                        const planetData = await response.json();
+
+                        const residentsData = await Promise.all(
+                            planetData.residents.map(async (url: string) => {
+                                const response = await fetch(url);
+
+                                if (!response.ok) {
+                                    throw new Error("Network response was not ok");
+                                }
+
+                                const resident = await response.json();
+                                const films = await fetchFilmDetails(resident.films);
+
+                                return {
+                                    name: resident.name,
+                                    films: films,
+                                }
+                            })
+                        );
+
+                        const residents: string[] = residentsData.map((residentData) => residentData.name);
+
+                        const planetResult = {
+                            name: planetData.name,
+                            population: planetData.population,
+                            climate: planetData.climate,
+                            residents: residents,
+                        };
+
+                        return { planet: planetResult, residents: residentsData };
+                    })
+                );
+                setSearchResult(planetsData);
+            }
+
+            let url = "";
+            if (searchType === "characters") {
+                url = `https://swapi.dev/api/people/?search=${searchTerm}`;
+            } else if (searchType === "planets") {
+                url = `https://swapi.dev/api/planets/?search=${searchTerm}`;
+            }
+
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
             const data = await response.json();
+            const [result] = data.results;
 
-            const filteredPlanets = data.results.filter((planet: any) => {
-                const planetPopulation = parseInt(planet.population);
 
-                return planetPopulation >= (minValue ?? 0) && (!maxValue || planetPopulation <= maxValue);
-            });
+            if (result) {
+                if (searchType === "characters") {
+                    const homeworldResponse = await fetch(result.homeworld);
 
-            const planetsData = await Promise.all(
-                filteredPlanets.map(async (planet: any) => {
-                    const response = await fetch(planet.url);
-
-                    if (!response.ok) {
-                        throw new Error("Network response was not ok");
+                    if (!homeworldResponse.ok) {
+                        throw new Error('Network response was not ok');
                     }
 
-                    const planetData = await response.json();
+                    const { name, population } = await homeworldResponse.json();
 
-                    const residentsData = await Promise.all(
-                        planetData.residents.map(async (url: string) => {
-                            const response = await fetch(url);
-
-                            if (!response.ok) {
-                                throw new Error("Network response was not ok");
-                            }
-
-                            const resident = await response.json();
-                            const films = await fetchFilmDetails(resident.films);
-
-                            return {
-                                name: resident.name,
-                                films: films,
-                            }
-                        })
-                    );
-
-                    const residents: string[] = residentsData.map((residentData) => residentData.name);
-
-                    const planetResult = {
-                        name: planetData.name,
-                        population: planetData.population,
-                        climate: planetData.climate,
-                        residents: residents,
+                    const characterData: Character = {
+                        name: result.name,
+                        homeworld: name,
+                        population: population,
+                        films: result.films,
                     };
 
-                    return { planet: planetResult, residents: residentsData };
-                })
-            );
-            setSearchResult(planetsData);
-        }
+                    const filmDetails: Film[] = await fetchFilmDetails(characterData.films);
 
-        try {
-            let url = "";
-        if (searchType === "characters") {
-            url = `https://swapi.dev/api/people/?search=${searchTerm}`;
-        } else if (searchType === "planets") {
-            url = `https://swapi.dev/api/planets/?search=${searchTerm}`;
-        }
-
-        const response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        const [result] = data.results;
-
-
-        if (result) {
-            if (searchType === "characters") {
-                const homeworldResponse = await fetch(result.homeworld);
-
-                if (!homeworldResponse.ok) {
-                    throw new Error('Network response was not ok');
-                }
-
-                const { name, population } = await homeworldResponse.json();
-
-                const characterData: Character = {
-                    name: result.name,
-                    homeworld: name,
-                    population: population,
-                    films: result.films,
-                };
-
-                const filmDetails: Film[] = await fetchFilmDetails(characterData.films);
-
-                setSearchResult({ character: characterData, films: filmDetails });
+                    setSearchResult({ character: characterData, films: filmDetails });
                 } else if (searchType === "planets") {
                     const residentsData = await Promise.all(
                         result.residents.map(async (url: string) => {
